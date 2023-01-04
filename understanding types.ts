e14c19dafd6fd7be0fc52579a8d1b354aa7a69af
Lance <lanceyu619@gmail.com>
2023-01-04 01:00:29 -0500

    next

---
+++ understanding types.ts
@@ -0,0 +1,8 @@
+const count = 452947234234;
+console.log("count", typeof count);
+const address = null;
+console.log("address", typeof address);
+const phone = undefined;
+console.log("phone", typeof phone);
+const stars = Symbol("***");
+console.log("stars", typeof stars);
